4dfac2444c25f836aece3808e4aa38d70ca41f31
Danny <dannynhansen@gmail.com>
2024-12-18 02:57:29 +0100

    Refactored theme selection + replaced button with dropdown.

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,27 +1,34 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const toggleButton = document.querySelector('.theme-selector');
-    const root = document.documentElement;
-    const imgs = document.querySelectorAll(".toggle");
-  
-    if (localStorage.getItem('theme') === 'dark') {
-      root.classList.add("dark");
-      imgs.forEach((img) => {
-        img.data = img.getAttribute('data-dark');
-      });
-    }
-  
-    toggleButton.addEventListener('click', () => {
-      root.classList.toggle("dark");
-      if (root.classList.contains('dark')) {
-        localStorage.setItem('theme', 'dark');
-        imgs.forEach((img) => {
-          img.data = img.getAttribute('data-dark');
-        });
-      } else {
-        localStorage.setItem('theme', 'light');
+  const themeSelector = document.querySelector('.theme-selector');
+  const root = document.documentElement;
+  const imgs = document.querySelectorAll(".toggle");
+  const css = document.querySelector("#dark-css");
+
+  function activateTheme(theme) {
+    localStorage.setItem('theme', theme);
+    themeSelector.value = theme;
+    switch (theme) {
+      case "light":
+        css.media = "not all";
         imgs.forEach((img) => {
           img.data = img.getAttribute('data-light');
         });
-      }
-    });
+        break;
+      case "dark":
+        css.media = "all";
+        imgs.forEach((img) => {
+          img.data = img.getAttribute('data-dark');
+        });
+        break;
+      default:
+        // auto
+        css.media = "(prefers-color-scheme: dark)";
+    }
+  }
+
+  activateTheme(localStorage.getItem('theme') || "light");
+
+  themeSelector.addEventListener("change", (event) => {
+    activateTheme(event.target.value);
+  });
 });
